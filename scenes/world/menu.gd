1703cfbc15b0fb8e757e26e6ce916cb79b5f0ab2
kacperekgamerek <kac.spyc@gmail.com>
2024-11-23 05:01:19 +0100

    Update menu.gd

--- scenes/world/menu.gd
+++ scenes/world/menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world/cave.tscn")
+	get_tree().change_scene_to_file("res://scenes/world/main.tscn")
 
 
 func _on_options_pressed() -> void:
