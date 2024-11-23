11f77f3c2d7fe37a39400c26c600d0a2069e8fc9
DeIta <kubab20072017@gmail.com>
2024-11-23 08:14:45 +0100

    r

--- scenes/world/menu.gd
+++ scenes/world/menu.gd
@@ -1,10 +1,20 @@
 extends Control
 
-func _ready() -> void:
-	$VBoxContainer/StartButton
+@onready var transition  = $Transition
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
 
-func _on_quit_button_pressed() -> void:
+func _on_start_button_pressed():
+	transition.play("fade_out")
+
+func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_options_button_pressed():
+	var options = load("res://scenes/world/options.tscn").instantiate()
+	get_tree().current_scene.add_child(options)
+
+func _on_transition_animation_finished(anim_name):
+	if anim_name == "fade_out":
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
--- scripts/options.gd
+++ scripts/options.gd
@@ -1,15 +1,7 @@
 extends Control
 
+func _on_back_pressed():
+		get_tree().current_scene.remove_child(self)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/world/menu.tscn")
+func _ready():
+	$VBoxContainer/Back.grab_focus()
