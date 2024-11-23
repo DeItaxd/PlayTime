27f2e0a795327a10e03274da31723857947980e6
DeIta <kubab20072017@gmail.com>
2024-11-23 03:08:39 +0100

    r

--- scripts/polygon.gd
+++ scripts/polygon.gd
@@ -5,15 +5,3 @@ extends StaticBody2D
 
 func _ready():
 	collision.polygon = polygon2d.polygon
-	collision.disabled = true
-	Signals.connect("forward", forward)
-	Signals.connect("backward", backward)
-	visible = false
-
-func forward():
-	visible = true
-	collision.disabled = false
-
-func backward():
-	visible = false
-	collision.disabled = true
