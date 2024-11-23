03d5f9a360bbc48816f092a7f3a28d237a89d440
DeIta <kubab20072017@gmail.com>
2024-11-23 03:56:11 +0100

    r

--- scripts/polygon.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-@onready var polygon2d = $Polygon2D
-@onready var collision = $CollisionPolygon2D
-
-func _ready():
-	collision.polygon = polygon2d.polygon
--- scripts/signals.gd
+++ scripts/signals.gd
@@ -1,4 +1,3 @@
 extends Node
 
-signal forward
-signal backward
+signal voided
